7ea0a53fb0dc0d5123be7c7ea9c79fe1bf8f96c9
Fausnightm <m.fausnight@lafilm.edu>
2021-08-07 17:45:12 -0400

    route files

--- server.js
+++ server.js
@@ -5,9 +5,15 @@ const PORT = process.env.PORT || 3001;
 const fs = require('fs')
 const path = require('path');
 
+const routesApi = require('./routes/routesApi')
+const routesHtml = require('./routes/routesHtml')
+
 app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use('/api', routesApi);
+app.use('/', routesHtml);
 
 
 app.listen(PORT, function() {
